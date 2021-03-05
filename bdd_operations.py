c02ee17d3217a19e53f11fb998d3599b8ddeb98a
LN_SniperKing <luke.c@laposte.net>
2021-03-05 15:52:48 +0100

    - create_bdd_folder - create_bdd_table - insert_objects_in_table

---
+++ bdd_operations.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+from tinydb import TinyDB
+from pathlib import Path
+
+from basics_operations import add_folder
+
+
+def create_bdd_folder():
+    base_dir_script = Path.cwd()
+    # GET 'BDD' FOLDER
+    try:
+        path_bdd_directory = add_folder(base_dir_script, 'BDD')
+    except:
+        path_bdd_directory = ""
+    return path_bdd_directory
+
+
+def create_bdd_table(path, name):
+    path_table = Path(path, name + ".json")
+
+    db = TinyDB(path_table)
+    players_table = db.table("players")
+    #players_table.truncate()  # clear the table first
+
+    return players_table
+
+
+def insert_objects_in_table(table, serialized_objects):
+    table.insert_multiple(serialized_objects)

7248ccc7c6a602a7a8d7efd79b109996fdc35321
LN_SniperKing <luke.c@laposte.net>
2021-03-06 09:41:16 +0100

    Add: - dialog box to confirm new player

--- controller.py
+++ controller.py
@@ -1,12 +1,10 @@
 # coding: utf-8
 
-from display_operations import clear, show_menu
-from ask_user_module import ask_user, date_time_controller
+from display_operations import print_dico_items, clear
+from inputs_operations import main_menu_actions, inputs_add_player, dialog_box_to_confirm_or_cancel
+
 from player_model import Player
 from db_operations import create_db_folder, create_db_table, insert_objects_in_table
-
-from inputs_operations import inputs_add_player
-# --- TOOLS ---
 
 def add_player():
     """ Contains all operations to add a new player in data base """
@@ -17,16 +15,24 @@ def add_player():
     player = Player(name, first_name, birthday, gender, ranking)
     serialized_player = player.serialized()
 
-    # CREATE OR ACCES TO DATA BASE
-    path_bdd = create_db_folder()
-    players_table = create_db_table(path_bdd, "Players")
+    # DIALOG BOX TO CONFIRM
+    clear()
+    confirm = dialog_box_to_confirm_or_cancel("Are you sure to add this player in database ?\n"
+                                              + print_dico_items(serialized_player))
+    if confirm:
+        # CREATE OR ACCES TO DATA BASE
+        path_bdd = create_db_folder()
+        players_table = create_db_table(path_bdd, "Players")
 
-    # INSERT OBJECT TO DATA BASE
-    insert_objects_in_table(players_table, [serialized_player])
+        # INSERT OBJECT TO DATA BASE
+        insert_objects_in_table(players_table, [serialized_player])
+    else:
+        main_menu_actions()
+
 
 def start_new_tournament():
-    clear()
     print("START NEW TOURNAMENT")
     #t = Tournament()
+
 
 add_player()

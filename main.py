dccc3b63cedc2b816dcc85023927a9ef61d88d06
LN_SniperKing <luke.c@laposte.net>
2021-03-06 09:39:02 +0100

    Remove: - main_menu_actions - menu_player_actions - menu_tournament_actions Edit: - Convert def main to class Main

--- main.py
+++ main.py
@@ -1,78 +1,17 @@
 # coding: utf-8
 
-import logging as lg
-
-from display_operations import show_menu
-from controller import add_player, start_new_tournament  # keep this line
-
-from ask_user_module import ask_user, date_time_controller
-
-# --- MENUS ---
+# import logging as lg
+from inputs_operations import main_menu_actions
 
 
-def main_menu_actions():
-    """ Display main menu and control calls to actions """
-    choice = "0"
-    choices = {"1": "- Enter 1 to acces Players Menu.\n",
-               "2": "- Enter 2 to acces Tournaments Menu\n",
-               "3": "- Enter 3 to start a new tournament\n"}
-
-    while choice not in list(choices):
-        choice = show_menu("MAIN - MENU",  ''.join(choices.values()))
-
-    if choice == "1":
-        menu_player_actions()
-    elif choice == "2":
-        menu_tournament_actions()
-    elif choice == "3":
-        start_new_tournament()
-
-
-def menu_player_actions():
-    """ Display players menu and control calls to actions """
-    choice = "0"
-    choices = {"1": "- Enter 1 to Add player\n",
-               "2": "- Enter 2 to Edit Player\n",
-               "3": "- Enter 3 to Remove Player\n",
-               "4": "- Enter 4 to Return in Main Menu\n"}
-
-    while choice not in choices:
-        choice = show_menu("PLAYER - MENU", ''.join(choices.values()))
-
-    if choice == "1":
-        menu_player_actions()
-    elif choice == "2":
-        menu_tournament_actions()
-    elif choice == "3":
-        start_new_tournament()
-    elif choice == "4":
-        main_menu_actions()
-
-
-def menu_tournament_actions():
-    """ Display tournament menu and control calls to actions """
-    choice = "0"
-    choices = {"1": "- Enter 1 to Edit Tournament\n",
-               "2": "- Enter 2 to Delete Tournament\n",
-               "4": "- Enter 4 to Return in Main Menu\n"}
-
-    while choice not in list(choices):
-        choice = show_menu("TOURNAMENT - MENU", ''.join(choices.values()))
-
-    if choice == "1":
-        menu_player_actions()
-    elif choice == "2":
-        menu_tournament_actions()
-    elif choice == "4":
-        main_menu_actions()
-
-
-def main():
+class Main:
     """ Run main menu """
-    lg.basicConfig(level=lg.DEBUG)
 
-    main_menu_actions()
+    def __init__(self):
+        # lg.basicConfig(level=lg.DEBUG)
+        main_menu_actions()
 
 
 if __name__ == '__main__':
-    main()
+    Main()
+

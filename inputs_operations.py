10d4e00cf613c05dbfa9fd55c2399919f19096d5
LN_SniperKing <luke.c@laposte.net>
2021-03-12 12:07:32 +0100

    Add : - inputs_add_tournament - tournament_dates_start_end_from_duration

--- inputs_operations.py
+++ inputs_operations.py
@@ -3,6 +3,8 @@
 from ask_user_module import ask_user, date_time_controller
 from display_operations import show_menu, clear
 
+from basics_operations import get_curent_date_time, date_type, convert_string_to_date_time, add_days_to_date_time,\
+    convert_date_time_to_string
 # --- MENUS ---
 
 
@@ -28,8 +30,8 @@ def menu_player_actions():
     """ Display players menu and control calls to actions """
     choice = "0"
     choices = {"1": "- Enter 1 to Add player\n",
-               "2": "- Enter 2 to Edit Player\n",
-               "3": "- Enter 3 to Remove Player\n",
+               # "2": "- Enter 2 to Edit Player\n",
+               # "3": "- Enter 3 to Remove Player\n",
                "4": "- Enter 4 to Return in Main Menu\n"}
 
     while choice not in choices:
@@ -37,10 +39,10 @@ def menu_player_actions():
 
     if choice == "1":
         menu_player_actions()
-    elif choice == "2":
-        menu_tournament_actions()
-    elif choice == "3":
-        menu_tournament_actions()
+    # elif choice == "2":
+    #     menu_tournament_actions()
+    # elif choice == "3":
+    #     menu_tournament_actions()
     elif choice == "4":
         main_menu_actions()
 
@@ -106,5 +108,49 @@ def inputs_add_player():
     ranking = ask_user('Enter ranking', int, "1")
 
     return name, first_name, birthday, gender, ranking
+
+
+def tournament_dates_start_end_from_duration(duration_value):
+    # INIT
+    dates = []
+    input_days = duration_value
+    date_string = get_curent_date_time()
+    format_date = "%Y-%m-%d %H:%M:%S"
+    format_date_stamp = format_date.replace("%", "")
+
+    user_input = ask_user('Enter tournament date start ' + format_date_stamp, str, date_string)
+    while type(user_input) != date_type():
+        user_input = convert_string_to_date_time(user_input)
+        if user_input is "":
+            print("Please write date in this format : " + + format_date_stamp)
+            user_input = ask_user('Enter tournament date start' + format_date_stamp, str, date_string)
+
+    # ADD ITEM(S) IN LIST
+    dates.append(convert_date_time_to_string(user_input))
+
+    if input_days > 1:
+        tournament_date_end = add_days_to_date_time(user_input, input_days)
+        dates.append(convert_date_time_to_string(tournament_date_end))
+    return dates
+
+
+def inputs_add_tournament():
+    name = ask_user('Enter name', str, "test")
+    place = ask_user('Enter place', str, "ici")
+
+    duration = ask_user('Enter tournament duration (on how many days)', int, "1")
+    dates = tournament_dates_start_end_from_duration(duration)
+
+    turns = ask_user('Enter number of turns', int, "4")
+    rounds = []
+
+    players_count = 8
+    players = []
+
+    time_control = ask_user("Enter 1 for Bullet.\nEnter 2 for Blitz.\nEnter 3 for Coup Rapide\n", int, "1")
+    description = ask_user('Enter a description', str, "blabla")
+
+    return name, place, duration, dates, turns, rounds, players_count, players, time_control, description
+
 
 

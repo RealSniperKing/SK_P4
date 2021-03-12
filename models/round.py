6f70921d5ff4097ae7e8f0adf361820c45bda5fd
LN_SniperKing <luke.c@laposte.net>
2021-03-12 12:52:33 +0100

    EDIT : return self

--- models/round.py
+++ models/round.py
@@ -6,10 +6,10 @@ import random
 
 
 def generate_fake_results():
-    dico = {0: [0.5, 0.5], 1: [1, 0], 2: [0, 1]}
+    array = [[0.5, 0.5], [1, 0], [0, 1]]
     random_value = random.randrange(0, 3)
 
-    return dico[random_value]
+    return array[random_value]
 
 
 class Round:
@@ -22,8 +22,8 @@ class Round:
 
     def start(self):
         self.date_start = get_curent_date_time()
-
         print("------ START ------ " + self.date_start)
+        return self
 
     def play(self, time_value):
         for match in self.list_matchs:
@@ -31,10 +31,12 @@ class Round:
             match.set_score_player_1(fake_result[0])
             match.set_score_player_2(fake_result[1])
         time.sleep(time_value)
+        return self
 
     def end(self):
         self.date_end = get_curent_date_time()
         print("------ END ------ " + self.date_end)
+        return self
 
     def round_results(self):
         players_scores = []

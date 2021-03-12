7ab01d312b58fd3cfaaf2db08f04cee5b3df25ed
LN_SniperKing <luke.c@laposte.net>
2021-03-12 11:58:59 +0100

    Edit and move class Tournament

---
+++ models/tournament_model.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+
+from datetime import datetime, date, timedelta
+from ask_user_module import ask_user
+import os
+from basics_operations import add_folder
+
+from tinydb import TinyDB
+
+class Tournament:
+    def __init__(self, name, place, duration, dates, turns, rounds, players_count, players, time_control, description):
+        # WHERE
+        self.name = name
+        self.place = place
+
+        # DATE(S)
+        self.duration = duration
+        self.dates = dates
+
+        # RULES
+        self.turns = turns
+        self.rounds = rounds
+
+        self.players_count = players_count
+        self.players = players
+
+        self.time_control = time_control
+        self.description = description
+
+        # EXPORT
+        self.serialized_tournament = None
+
+    def serialized(self):
+        self.serialized_tournament = {"name": self.name, "place": self.place, "duration": self.duration,
+                                      "dates": self.dates, "turns": self.turns, "rounds": self.rounds,
+                                      "players_count": self.players_count, "players": self.players,
+                                      "time_control": self.time_control, "description": self.description}
+        return self.serialized_tournament

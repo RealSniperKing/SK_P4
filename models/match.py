7201a84de7fe7a390f8c1ec5bb198a3881d14ae3
LN_SniperKing <luke.c@laposte.net>
2021-03-17 19:18:18 +0100

    Add serialized_infos

--- models/match.py
+++ models/match.py
@@ -8,8 +8,21 @@ class Match:
 
         self.datas = (list1_player_score, list2_player_score)
 
+        self.serialized_matchs = []
+
     def set_score_player_1(self, value):
         self.list1_player_score[1] = value
 
     def set_score_player_2(self, value):
         self.list2_player_score[1] = value
+
+    def serialized_infos(self):
+        infos = []
+        for data in self.datas:
+            player_object = data[0]
+            player_score = data[1]
+
+            dico = {"player_object": player_object, "player_score": player_score,
+                    "player_ranking": player_object.ranking}
+            infos.append(dico)
+        return infos

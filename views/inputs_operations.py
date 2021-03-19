9f35bead494d7740899849d0fd16a506432c7287
LN_SniperKing <luke.c@laposte.net>
2021-03-19 12:11:35 +0100

    Remove : - main_menu_actions - menu_player_actions - menu_tournament_actions

--- views/inputs_operations.py
+++ views/inputs_operations.py
@@ -8,63 +8,6 @@ from basics_operations import get_curent_date_time, date_type, convert_string_to
 # --- MENUS ---
 
 
-def main_menu_actions():
-    """ Display main menu and control calls to actions """
-    choice = "0"
-    choices = {"1": "- Enter 1 to acces Players Menu.\n",
-               "2": "- Enter 2 to acces Tournaments Menu\n",
-               "3": "- Enter 3 to start a new tournament\n"}
-
-    while choice not in list(choices):
-        choice = show_menu("MAIN - MENU",  ''.join(choices.values()))
-
-    if choice == "1":
-        menu_player_actions()
-    elif choice == "2":
-        menu_tournament_actions()
-    elif choice == "3":
-        menu_tournament_actions()
-
-
-def menu_player_actions():
-    """ Display players menu and control calls to actions """
-    choice = "0"
-    choices = {"1": "- Enter 1 to Add player\n",
-               # "2": "- Enter 2 to Edit Player\n",
-               # "3": "- Enter 3 to Remove Player\n",
-               "4": "- Enter 4 to Return in Main Menu\n"}
-
-    while choice not in choices:
-        choice = show_menu("PLAYER - MENU", ''.join(choices.values()))
-
-    if choice == "1":
-        menu_player_actions()
-    # elif choice == "2":
-    #     menu_tournament_actions()
-    # elif choice == "3":
-    #     menu_tournament_actions()
-    elif choice == "4":
-        main_menu_actions()
-
-
-def menu_tournament_actions():
-    """ Display tournament menu and control calls to actions """
-    choice = "0"
-    choices = {"1": "- Enter 1 to Edit Tournament\n",
-               "2": "- Enter 2 to Delete Tournament\n",
-               "4": "- Enter 4 to Return in Main Menu\n"}
-
-    while choice not in list(choices):
-        choice = show_menu("TOURNAMENT - MENU", ''.join(choices.values()))
-
-    if choice == "1":
-        menu_player_actions()
-    elif choice == "2":
-        menu_tournament_actions()
-    elif choice == "4":
-        main_menu_actions()
-
-
 def dialog_box_to_confirm_or_cancel(title):
     choice = "0"
     choices = {"1": "- Enter 1 to Confirm\n",

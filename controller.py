2368768b2411406fa383ed38e79bf210741b0cc8
LN_SniperKing <luke.c@laposte.net>
2021-03-25 09:34:08 +0100

    Add : - analyze_tournaments - show_rounds_result

--- controller.py
+++ controller.py
@@ -12,6 +12,8 @@ from models.class_database import create_db_folder, Database
 from controllers.algorithm import AlgoSuisse
 from models.class_round import Round
 
+import os
+import os.path
 
 # DATABASE --------------------------------------------------
 def add_player_in_db():
@@ -47,12 +49,12 @@ def add_tournament_in_db():
     """ Contains all operations to add a new tournament in data base """
 
     # USER INPUTS
-    name, place, duration, dates, turns, rounds, players_count, \
-    players, time_control, description = inputs_add_tournament()
+    name, place, duration, dates, turns, rounds, players, \
+    time_control, description = inputs_add_tournament()
 
     # CREATE TOURNAMENT OBJECT
-    tournament = Tournament(name, place, duration, dates, turns, rounds, players_count, \
-    players, time_control, description)
+    tournament = Tournament(name, place, duration, dates, turns, rounds,
+                            players, time_control, description)
 
     serialized_tournament = tournament.serialized()
 
@@ -74,15 +76,36 @@ def add_tournament_in_db():
         database_object.insert_serialized_objects_in_current_table([serialized_tournament])
 
 
+def analyze_tournaments(table_name):
+    # CREATE OR LOAD DATABASE
+    path_bdd = create_db_folder()
+
+    choices = {}
+    if os.path.isdir(path_bdd):
+        database_object = Database(path_bdd, "database").create_or_load_table_name(table_name)
+
+        tournaments = database_object.get_all_items_in_current_table()
+
+        empty_tournaments = []
+        for st in tournaments:
+            tournament = Tournament(st["name"], st["place"], st["duration"],
+                                    st["dates"], st["turns"], st["rounds"],
+                                    st["players"], st["time_control"],
+                                    st["description"])
+            if not tournament.rounds:
+                empty_tournaments.append(tournament)
+
+        for i, t in enumerate(empty_tournaments, 1):
+            choices[str(i)] = "- Enter " + str(i) + " to run : " + t.name + " | " + t.place + "\n"
+
+    return choices
+
+
 def search_element_in_db():
     # CREATE OR ACCES TO DIRECTORY
     path_bdd = create_db_folder()
 
-    # CREATE OR LOAD DATABASE
-    database_object = Database(path_bdd, "database")
-
-    # CREATE OR LOAD PLAYERS
-    database_object.create_or_load_table_name("players")
+    database_object = Database(path_bdd, "database").create_or_load_table_name("tournaments")
 
     # SEARCH ITEM IN TABLE
     database_object.search_item_in_table('L+')
@@ -94,29 +117,29 @@ def start_new_tournament():
     print("START NEW TOURNAMENT")
     #t = Tournament()
 
+def show_rounds_result(rounds):
+    for round in rounds:
+        players_list = []
+        for match in round.matchs():
+            p_one, p_two = match.serialized_infos()
+            players_list.append(p_one)
+            players_list.append(p_two)
 
-def show_round_result(round):
-    players_list = []
-    for match in round.matchs():
-        p_one, p_two = match.serialized_infos()
-        players_list.append(p_one)
-        players_list.append(p_two)
+        list_results = []
+        for player in players_list:
+            dico_print = {}
+            dico_print["Round"] = round.name
+            dico_print["Name"] = player["player_object"].name
+            dico_print["First name"] = player["player_object"].firstname
 
-    list_results = []
-    for player in players_list:
-        dico_print = {}
-        dico_print["Round"] = round.name
-        dico_print["Name"] = player["player_object"].name
-        dico_print["First name"] = player["player_object"].firstname
+            dico_print["Tournament ranking"] = player["player_object"].tournament_ranking
+            dico_print["Before this round"] = player["player_object"].tournament_ranking - player["player_score"]
+            dico_print["Player Score"] = player["player_score"]
 
-        dico_print["Tournament ranking"] = player["player_object"].tournament_ranking
-        dico_print["Before this round"] = player["player_object"].tournament_ranking - player["player_score"]
-        dico_print["Player Score"] = player["player_score"]
+            dico_print["Default ranking"] = player["player_object"].ranking
+            list_results.append(dico_print)
 
-        dico_print["Default ranking"] = player["player_object"].ranking
-        list_results.append(dico_print)
-
-    convert_dico_to_df(list_results)
+        convert_dico_to_df(list_results)
 
 
 def start_game():
@@ -130,7 +153,7 @@ def start_game():
     # TODO OPTION TO CHOICE THE TOURNAMENT
     st = serialized_tournaments[0]
     tournament = Tournament(st["name"], st["place"], st["duration"], st["dates"], st["turns"], st["rounds"],
-                            st["players_count"], st["players"], st["time_control"], st["description"])
+                            st["players"], st["time_control"], st["description"])
 
     #database_object.remove_item(tournament.name)
 
@@ -146,12 +169,11 @@ def start_game():
 
     # ROUND1
     first_round = Round(matchs, "Round 1")
-    # show_round_result(first_round)
 
     tournament.add_round_in_rounds(first_round)
     first_round.start().play(0).end()
 
-    show_round_result(first_round)
+    show_rounds_result([first_round])
 
     # OTHERS ROUNDS
     last_round = first_round
@@ -165,12 +187,11 @@ def start_game():
         tournament.add_round_in_rounds(new_round)
         new_round.start().play(0).end()
 
-        show_round_result(new_round)
-
         rounds_count = len(tournament.rounds)
 
+    show_rounds_result(tournament.rounds)
     # print("write tournament")
     # print(tournament.serialized())
     # database_object.create_or_load_table_name('tournaments').update_item(tournament.name, tournament.serialized())
 
-#start_game()
+#analyze_tournaments("tournaments")
